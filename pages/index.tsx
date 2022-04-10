36723152864eb3ffd1e28a84ad662c285397c7c5
seongguk.yoon <seongguk.yoon@ontacthealth.com>
2022-04-10 14:43:08 +0900

    TECH-main Tailwind Into step 3nd   Modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+    <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item </span>
         <div className="flex justify-between my-2">
@@ -18,9 +18,12 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semibold">$27</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
+        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl   w-full mx-auto
+        hover:bg-teal-500 hover:text-gray-500
+        active:bg-yellow-500 focus:bg-red-500
+        ">
           Cheack Out
-        </div>
+        </button>
       </div>
 
       {/* pro file */}
@@ -42,15 +45,51 @@ const Home: NextPage = () => {
             </div>
           </div>
 
-          <div className="relative flex flex-col -mt-10 items-center  -mb-5" >
-            <span className='font-medium '>Seong Guk</span>
-            <span className='text-sm text-gray-500 '>Korea</span>
+          <div className="relative flex flex-col -mt-10 items-center  -mb-5">
+            <span className="font-medium ">Seong Guk</span>
+            <span className="text-sm text-gray-500 ">Korea</span>
           </div>
         </div>
       </div>
 
-      <div className="bg-white p-10 rounded-2xl mb-10 shadow"></div>
-      <div className="bg-white p-10 rounded-2xl mb-10 shadow"></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl py-5 px-5">
+        <div className="flex mb-5 justify-between items-center ">
+          <div>
+            <span>⬅</span>
+          </div>
+
+          <div className="space-x-3">
+            <span>⭐️ 4.8</span>
+            <span className="shadow-xl p-2 rounded-md">♥️</span>
+          </div>
+        </div>
+
+        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="flex  flex-col  ">
+          <span className="font-medium mb-1.5 text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          
+          <div className='flex  justify-between  items-center  mt-3 mb-8 '>
+            <div className="space-x-2">
+              <button className='w-5 h-5  rounded-full  focus:ring-2 ring-offset-2 ring-yellow-500 bg-yellow-500 transition' />
+              <button className='w-5 h-5  rounded-full  focus:ring-2 ring-offset-2 ring-indigo-500 bg-indigo-500 transition' />
+              <button className='w-5 h-5  rounded-full  focus:ring-2 ring-offset-2 ring-teal-500 bg-teal-500 transition' />      
+            </div>
+
+            <div className="flex items-center space-x-5 " >
+              <button className=' bg-blue-200 rounded-lg aspect-square w-8  text-xl text-gray-500 '>-</button>
+              <span>1</span>
+              <button className=' bg-blue-200 rounded-lg  aspect-square w-8  text-xl text-gray-500 '>+</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='flex  justify-between  items-center'>
+          <span className=' font-medium  text-2xl '>$ 450</span>
+          <button className='  bg-blue-500 text-center rounded-md p-2 px-8 text-ss text-white'> Add to cart </button>
+        </div>
+
+      </div>
     </div>
   );
 };
