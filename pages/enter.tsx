ec8d30ceb1d58c12733e18528a1ea16207903fdb
seongguk.yoon <seongguk.yoon@ontacthealth.com>
2022-04-27 16:05:17 +0900

    TECH-Tailwind Practice step_2nd - upload page

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -42,13 +42,14 @@ export default function Enter() {
         </div>
 
         <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700 ">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700 ">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-2">
             {method === "email" ? (
               <input
+                id="input"
                 className=" appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder:bg-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                 type="email"
                 required
@@ -60,6 +61,7 @@ export default function Enter() {
                   +82
                 </span>
                 <input
+                  id="input"
                   className=" appearance-none rounded-l-none px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder:bg-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                   type="number"
                   required
@@ -83,7 +85,7 @@ export default function Enter() {
           </div>
           {/* 트위터, 깃허브 아이콘 */}
           <div className="grid grid-cols-2 gap-3 mt-2">
-            <button className='flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white font-medium text-gray-500  hover:bg-gray-50'>
+            <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white font-medium text-gray-500  hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -93,7 +95,7 @@ export default function Enter() {
                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </button>
-            <button className='flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white font-medium text-gray-500  hover:bg-gray-50'>
+            <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white font-medium text-gray-500  hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -22,25 +22,25 @@ const Upload: NextPage = () => {
           <input className="hidden" type="file" />
         </label>
       </div>
-      <div>
-        <label htmlFor='price'>Price</label>
-        <div>
-          <div>
-            <span>$</span>
+      <div className='my-5'>
+        <label className="mb-1 block text-sm font-medium text-gray-700 " htmlFor='price'>Price</label>
+        <div className=' rounded-md shadow-sm  relative flex items-center'>
+          <div className=' absolute left-0 pl-3 flex items-center pointer-events-none justify-center'>
+            <span className='text-gray-500 text-sm'>$</span>
           </div>
-          <input id="price" type="text" placeholder="0.00" />
-          <div>
-            <span>USD</span>
+          <input id="price" className=' appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500' type="text" placeholder="0.00" />
+          <div className=' absolute right-0 pr-3 pointer-events-none flex items-center'>
+            <span className='text-gray-500'>USD</span>
           </div>
         </div>
       </div>
       <div>
-        <label>Description</label>
-        <div>
-          <textarea rows={4} />
-        </div>
+        <label className="mb-1 block text-sm font-medium text-gray-700 ">Description</label>
+        
+          <textarea className='mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300  focus:border-orange-500' rows={4} />
+        
       </div>
-      <button>Upload product</button>
+      <button  className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">Upload product</button>
     </div>
   );
 };
