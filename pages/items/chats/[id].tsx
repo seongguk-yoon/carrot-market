27f96cc29825d6115d74b5384c87b758ec21efba
seongguk.yoon <seongguk.yoon@ontacthealth.com>
2022-04-27 18:29:52 +0900

    TECH-Tailwind Practice step_7nd - Chat Detail

---
+++ pages/items/chats/[id].tsx
@@ -0,0 +1,67 @@
+import type { NextPage } from "next";
+
+const ChatDetail: NextPage = () => {
+  return (
+    <div className="py-10 px-4 space-y-6">
+      <div className="flex items-start space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border  border-gray-300 rounded-md">
+          <p>얼마에 파시나요 혹시 내고 가능한가요?</p>
+        </div>
+      </div>
+      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border  border-gray-300 rounded-md">
+          <p>2만원에 쿨거래 하시죠</p>
+        </div>
+      </div>
+      <div className="flex items-start space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border  border-gray-300 rounded-md">
+          <p>좋읍니다 ..</p>
+        </div>
+      </div>
+      <div className="fixed w-full mx-auto max-w-md bottom-2 left-0 right-0 inset-x-0">
+        <div className="flex items-center ">
+          <input
+            className="relative shadow-sm rounded-full w-full border-gray-300  focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+            type="text"
+          />
+          <div className=" absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <button className="flex items-center focus:ring-2 focus:ring-offset-2  focus:ring-orange-500 hover:bg-orange-600 cursor-pointer bg-orange-500  rounded-full px-3 text-sm text-white">
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border  border-gray-300 rounded-md">
+          <p>어디서볼까용?</p>
+        </div>
+      </div>
+      <div className="flex items-start space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-300" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border  border-gray-300 rounded-md">
+          <p>다음에 거래하시죠 ..</p>
+        </div>
+      </div>
+      <div className="fixed w-full mx-auto max-w-md bottom-2 left-0 right-0 inset-x-0">
+        <div className="flex items-center ">
+          <input
+            className="relative shadow-sm rounded-full w-full border-gray-300  focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+            type="text"
+          />
+          <div className=" absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <button className="flex items-center focus:ring-2 focus:ring-offset-2  focus:ring-orange-500 hover:bg-orange-600 cursor-pointer bg-orange-500  rounded-full px-3 text-sm text-white">
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDetail;
