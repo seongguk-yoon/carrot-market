5bbb02663f5701fcdfd9bb1544d7c0bb100ba1a1
seongguk.yoon <seongguk.yoon@ontacthealth.com>
2022-05-04 15:17:52 +0900

    TECH-Auth part1_2nd

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,61 +6,23 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone } = req.body;
   const payload = phone ? { phone: +phone } : { email };
 
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "TokenGguk",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Gguk",
-      ...payload,
-    },
-
-    update: {},
   });
-  console.log(user);
-
-  // if (phone) {
-  //   user = await client.user.upsert({
-  //     where: {
-  //       phone: +phone,
-  //     },
-  //     create: {
-  //       name: "seong",
-  //       phone: +phone,
-  //     },
-  //     update: {},
-  //   });
-  // } else if (email) {
-  //   user = await client.user.upsert({
-  //     where: {
-  //       email,
-  //     },
-  //     create: {
-  //       name: "네이놈",
-  //       email,
-  //     },
-  //     update: {},
-  //   });
-  // }
-
-  // if (email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-  //   if (user) console.log("이미유저네요?");
-  //   if (!user) {
-  //     console.group("없읍니다. 생성완료요~");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "seongguk",
-  //         email,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
+  console.log(token);
 
   // if (phone) {
   //   user = await client.user.findUnique({
