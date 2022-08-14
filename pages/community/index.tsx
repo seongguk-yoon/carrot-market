52cfdfabbe03eb63fce8101b447b8b7272810c6c
seongguk.yoon <seongguk.yoon@ontacthealth.com>
2022-08-14 17:54:29 +0900

    Profile step_1nd. 리뷰, 프로필 조회 관련 API prisma db 추가

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -32,7 +32,7 @@ const Community: NextPage = () => {
       <div className="space-y-4 divide-y-[1px]">
         {data?.posts?.map((post) => (
           <Link key={post.id} href={`/community/${post.id}`}>
-            <a className="flex cursor-pointer flex-col pt-4 items-start">
+            <a className="flex cursor-pointer flex-col pt-4 items-start ">
               <span className="flex ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
                 동네질문
               </span>
